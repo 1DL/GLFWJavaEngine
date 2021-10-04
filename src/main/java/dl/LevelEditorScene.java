8641c2ee0d99585d34fd1ad3a78564687acc7bf1
Luiz Henrique <tecnologia@m3solutions.com.br>
2021-10-04 14:57:25 -0300

    Implemented Data Oriented Design - Entity Component Pattern

--- src/main/java/dl/LevelEditorScene.java
+++ src/main/java/dl/LevelEditorScene.java
@@ -1,5 +1,7 @@
 package dl;
 
+import components.FontRenderer;
+import components.SpriteRenderer;
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import renderer.Shader;
@@ -19,10 +21,10 @@ public class LevelEditorScene extends Scene {
 
     private float[] vertexArray = {
         //position                      //color                     // UV Coordinates
-        100.0f  , 0.0f  , 0.0f,         1.0f, 0.0f, 0.0f, 1.0f,     1, 0,   //Bottom Right
-        0.0f    , 100.0f, 0.0f,         0.0f, 1.0f, 0.0f, 1.0f,     0, 1,   //top left
-        100.0f  , 100.0f, 0.0f,         1.0f, 0.0f, 1.0f, 1.0f,     1, 1,   //top right
-        0.0f    , 0.0f  , 0.0f,         1.0f, 1.0f, 0.0f, 1.0f,     0, 0,   //Bottom left
+        100.0f  , 0.0f  , 0.0f,         1.0f, 0.0f, 0.0f, 1.0f,     1, 0,   //Bottom Right  0
+        0.0f    , 100.0f, 0.0f,         0.0f, 1.0f, 0.0f, 1.0f,     0, 1,   //top left      1
+        100.0f  , 100.0f, 0.0f,         1.0f, 0.0f, 1.0f, 1.0f,     1, 1,   //top right     2
+        0.0f    , 0.0f  , 0.0f,         1.0f, 1.0f, 0.0f, 1.0f,     0, 0,   //Bottom left   3
     };
 
     //IMPORTANT: Must be in counter-clockwise order
@@ -43,12 +45,21 @@ public class LevelEditorScene extends Scene {
     private Shader defaultShader;
     private Texture testTexture;
 
+    GameObject testObj;
+    private boolean firstTime = false;
+
     public LevelEditorScene() {
 
     }
 
     @Override
     public void init() {
+        System.out.println("Creating 'test object'");
+        this.testObj = new GameObject("test object");
+        this.testObj.addComponent(new SpriteRenderer());
+        this.testObj.addComponent(new FontRenderer());
+        this.addGameObjectToScene(this.testObj);
+
         this.camera = new Camera(new Vector2f());
         defaultShader = new Shader("assets/shaders/default.glsl");
         defaultShader.compile();
@@ -131,5 +142,17 @@ public class LevelEditorScene extends Scene {
         glBindVertexArray(0);
 
         defaultShader.detach();
+
+        if (!firstTime) {
+            System.out.println("Creating gameObject!");
+            GameObject go = new GameObject("Game Test 2");
+            go.addComponent(new SpriteRenderer());
+            this.addGameObjectToScene(go);
+            firstTime = true;
+        }
+
+        for (GameObject go : this.gameObjects) {
+            go.update(dt);
+        }
     }
 }
